d1dfdad9beca91630f5221d6fb50558bda79c32c
Bas Wagenmaker <b.wagenmaker@inshared.nl>
2023-10-24 10:43:22 +0200

    Work in progress

--- src/components/board/board/board.tsx
+++ src/components/board/board/board.tsx
@@ -1,42 +1,34 @@
-import { useState } from "react";
 import { Cell } from "..";
 
 interface BoardProps {
   puzzle: number[][];
+  changeActiveCell: any;
+  activeCell: [number, number];
 }
 
-export const Board = ({ puzzle }: BoardProps) => {
-  const [activeCell, setActiveCell] = useState([null, null]);
-  const handleClick = (rowIndex, columnIndex) => {
-    if (activeCell[0] === rowIndex && activeCell[1] === columnIndex)
-      setActiveCell([null, null]);
-    else setActiveCell([rowIndex, columnIndex]);
-  };
+export const Board = ({ puzzle, changeActiveCell, activeCell }: BoardProps) => {
   return (
-    <>
-      <div className="container drop-shadow-md">
-        <table>
-          <tbody>
-            {puzzle.map((row: number[], rowIndex: number) => (
-              <tr key={rowIndex}>
-                {row.map((cell: number, columnIndex: number) => (
-                  <Cell
-                    value={cell}
-                    rowIndex={rowIndex}
-                    columnIndex={columnIndex}
-                    key={columnIndex + rowIndex * 9}
-                    onClick={handleClick}
-                    active={
-                      activeCell[0] === rowIndex &&
-                      activeCell[1] === columnIndex
-                    }
-                  />
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+    <div className="container w-96 xl:w-auto drop-shadow-md">
+      <table className="mx-auto">
+        <tbody>
+          {puzzle.map((row: number[], rowIndex: number) => (
+            <tr key={rowIndex}>
+              {row.map((cell: number, columnIndex: number) => (
+                <Cell
+                  value={cell}
+                  rowIndex={rowIndex}
+                  columnIndex={columnIndex}
+                  key={columnIndex + rowIndex * 9}
+                  onClick={changeActiveCell}
+                  active={
+                    activeCell[0] === rowIndex && activeCell[1] === columnIndex
+                  }
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
--- src/components/board/cell/cell.tsx
+++ src/components/board/cell/cell.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 interface CellProps {
   value: number;
   rowIndex: number;
@@ -14,14 +15,33 @@ export function Cell({
   active,
 }: CellProps) {
   const index = columnIndex + rowIndex * 9;
+
+  const cellClasses = classNames({
+    "border": true,
+    "border-slate-200": true,
+    "border-r-4": [2, 5].includes(columnIndex),
+    "border-b-4": [2, 5].includes(rowIndex),
+    "bg-slate-200": active,
+    "bg-white": !active,
+    "font-semibold": true,
+    "text-slate-700": true,
+    "text-lg": true,
+    "xl:w-16": true,
+    "xl:h-16": true,
+    "w-10": true,
+    "h-10": true,
+    "text-center": true,
+    "hover:bg-slate-100": true,
+    "hover:cursor-pointer": true,
+    "rounded-tl-md": rowIndex === 0 && columnIndex === 0,
+    "rounded-tr-md": rowIndex === 0 && columnIndex === 8,
+    "rounded-bl-md": rowIndex === 8 && columnIndex === 0,
+    "rounded-br-md": rowIndex === 8 && columnIndex === 8,
+  });
+
   return (
-    <td
-      className={`border-2 w-12 h-12 text-center bg-white hover:bg-slate-100 hover:cursor-pointer ${
-        active ? "bg-slate-100" : "bg-white"
-      }`}
-      onClick={() => onClick(rowIndex, columnIndex)}
-    >
-      {value}
+    <td className={cellClasses} onClick={() => onClick(rowIndex, columnIndex)}>
+      {value > 0 ? value : ""}
     </td>
   );
 }
